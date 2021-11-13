4ae01a9a8524b373087827ddf16025784913f2a7
Robert Eads <SkintightShoe72@gmail.com>
2021-11-12 21:45:33 -0600

    Updated custom logging and added unit tests

--- custom_logging.py
+++ custom_logging.py
@@ -2,31 +2,40 @@
 
 from datetime import datetime
 
-#Assuming logging file is open for all functions
+def open_file(file_name = "log.txt"):
+    global log_file
+    log_file = open(file_name,"a")
+    log_info("Successfully opened log file")
+
+
+def close_file():
+    log_info("Closing log file")
+    log_file.close()
+
 
 #Custom event logging
-def log_event(log_file, message, error_type, log_console = False):
+def log_event(message, error_type, log_console = False):
     log_file.write("{0}[{1}] {2}\n".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), error_type, message))
     if log_console:
         print("{0}[{1}] {2}".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), error_type, message))
 
 #Basic Logging
-def log_error(log_file, message, log_console = False):
+def log_error(message, log_console = False):
     log_file.write("{0}[ERROR] {1}\n".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), message))
     if log_console:
         print("{0}[ERROR] {1}".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), message))
 
-def log_warning(log_file, message, log_console = False):
+def log_warning(message, log_console = False):
     log_file.write("{0}[WARNING] {1}\n".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), message))
     if log_console:
         print("{0}[WARNING] {1}".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), message))
 
-def log_info(log_file, message, log_console = False):
+def log_info(message, log_console = False):
     log_file.write("{0}[INFO] {1}\n".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), message))
     if log_console:
         print("{0}[INFO] {1}".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), message))
 
-def log_debug(log_file, message, log_console = False):
+def log_debug(message, log_console = False):
     log_file.write("{0}[DEBUG] {1}\n".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), message))
     if log_console:
         print("{0}[DEBUG] {1}".format((datetime.now().strftime("[%m-%d-%Y][%H:%M:%S]")), message))
---
+++ test_custom_logging.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3.9
+
+import custom_logging as log
+
+def test_open_file():
+    log.open_file()
+
+def test_log_error():
+    log.log_error("No internet connection - Console Print", True)
+    log.log_error("No internet connection")
+
+    log.log_warning("Link is broken - Console Print", True)
+    log.log_warning("Link is broken")
+
+    log.log_debug("No idea what this is for - Console Print", True)
+    log.log_debug("No idea what this is for")
+    
+    log.log_info("task ran successfully - Console Print", True)
+    log.log_info("task ran successfully")
+    
+    log.log_event("This is just a test - Console Print", "TEST", True)
+    log.log_event("This is just a test", "TEST")
+
+def test_close_file():
+    log.close_file()
+
